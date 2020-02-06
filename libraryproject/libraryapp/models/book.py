0c9612ae2e7de1ae858bd11081c0f91036c06841
Caroline Brownlee <brownleecaroline@gmail.com>
2020-02-06 14:59:26 -0600

    made migrations for models

--- libraryproject/libraryapp/models/book.py
+++ libraryproject/libraryapp/models/book.py
@@ -14,14 +14,14 @@ class Book(models.Model):
 
     title = models.CharField(max_length=50)
     isbn_number = models.CharField(max_length=50) 
-    author = models.IntegerField(max_length=50)
-    year_published = models.IntegerField(max_length=50)
+    author = models.CharField(max_length=50)
+    year_published = models.IntegerField()
     location = models.ForeignKey(Library, on_delete=models.CASCADE)
     librarian = models.ForeignKey(Librarian, on_delete=models.CASCADE)
 
     class Meta:
-        verbose_name = _("")
-        verbose_name_plural = _("s")
+        verbose_name = ("Book")
+        verbose_name_plural = ("Books")
 
     def __str__(self):
         return self.title
