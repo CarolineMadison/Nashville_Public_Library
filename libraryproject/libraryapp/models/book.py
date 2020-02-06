18d9d6df9bd93c5a1a83cea10fe32b2551c8a5e6
Caroline Brownlee <brownleecaroline@gmail.com>
2020-02-05 23:30:33 -0600

    added code to librarian.py

---
+++ libraryproject/libraryapp/models/book.py
@@ -0,0 +1,28 @@
+# Lightning Exercise: It's your job to create a Book model in the book.py module. A book should have the following properties.
+
+# Title (CharField)
+# ISBN number (CharField)
+# Author (CharField)
+# Year published (IntegerField)
+
+# This import is needed for Python classes that are modeling a database table. (NSS, Chapter Documentation)
+from django.db import models
+from .library import Library
+
+class Book(models.Model):
+
+    title = models.CharField(max_length=50)
+    isbn_number = models.CharField(max_length=50) 
+    author = models.IntegerField(max_length=50)
+    year_published = models.IntegerField(max_length=50)
+    location = models.ForeignKey(Library, on_delete=models.CASCADE)
+ 
+    class Meta:
+        verbose_name = _("")
+        verbose_name_plural = _("s")
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("_detail", kwargs={"pk": self.pk})
--- libraryproject/libraryapp/models/library.py
+++ libraryproject/libraryapp/models/library.py
@@ -3,12 +3,15 @@ from django.db import models
 
 class Library(models.Model):
 
+    title = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+
     class Meta:
         verbose_name = ("Library")
         verbose_name_plural = ("Librarys")
 
     def __str__(self):
-        return self.name
+        return self.title
 
     def get_absolute_url(self):
         return reverse("Library_detail", kwargs={"pk": self.pk})
