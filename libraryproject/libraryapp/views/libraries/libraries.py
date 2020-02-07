416018ec6fc17d6efba20d36e0a08ca9c52bd518
Caroline Brownlee <brownleecaroline@gmail.com>
2020-02-07 15:58:37 -0600

    put code into views/libraries/list.py

---
+++ libraryproject/libraryapp/views/libraries/libraries.py
@@ -0,0 +1,48 @@
+import sqlite3
+from django.shortcuts import render
+from libraryapp.models import Library
+from ..connection import Connection
+
+def library_list(request):
+    if request.method == 'GET':
+        with sqlite3.connect(Connection.db_path) as conn:
+            
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select
+                l.id,
+                l.title,
+                l.address 
+            from libraryapp_library l
+            """)
+
+            # When you instruct the sqlite3 package to fetchall(), it takes your SQL string and walks over to the database and executes it. It then takes all of the rows that the database generates, and creates a tuple out of each one. It then puts all of those tuples into a list. (Chapter Documentation, NSS)
+
+            # TUPLE is a collection which is ordered and unchangeable. Allows duplicate members. Parenthesis. (W3 Schools)
+
+            # LIST is a collection which is ordered and changeable. Allows duplicate members. Brackets. (W3 Schools)
+
+            all_libraries = []
+            dataset = db_cursor.fetchall()
+
+            for row in dataset:
+                # creating an instance of a library with Library()
+                library = Library()
+                library.id = row['id']
+                library.title = row['title']
+
+                all_libraries.append(library)
+
+        # When a view wants to generate some HTML representations of data, it needs to specify a template to use. [Below], the template variable is holding the path and filename of the template. (Nashville Software School, Ch 3 Documentation)
+
+        template = 'libraries/list.html'
+
+        # the dictionary 'all_libraries' has a single property labeled all_libraries and its value is the list of library objects that the view generates. // The key name is able to be used in a loop in the template.  (Nashville Software School, Ch 3 Documentation)
+
+        context = {
+            'all_libraries': all_libraries
+        }
+        # Then the render() method is invoked. That method takes the HTTP request as the first argument, the template to be used as the second argument, and then a dictionary containing the data to be used in the template. (Nashville Software School, Ch 3 Documentation)
+        return render(request, template, context)
