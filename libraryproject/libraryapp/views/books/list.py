a6c897155a136f8c66823710c4dee027ea87ab28
Caroline Brownlee <brownleecaroline@gmail.com>
2020-02-06 20:08:02 -0600

    wrote code into list.html in template/books, commented out code there and corresponding code in views/books/list.py

--- libraryproject/libraryapp/views/books/list.py
+++ libraryproject/libraryapp/views/books/list.py
@@ -45,9 +45,14 @@ def book_list(request):
 
                 all_books.append(book)
 
+        # When a view wants to generate some HTML representations of data, it needs to specify a template to use. [Below], the template variable is holding the path and filename of the template. (Nashville Software School, Ch 3 Documentation)
+
         template = 'books/list.html'
+
+        # the dictionary 'all_books' has a single property labeled all_books and its value is the list of book objects that the view generates. // The key name is able to be used in a loop in the template.  (Nashville Software School, Ch 3 Documentation)
+
         context = {
             'all_books': all_books
         }
-
+        # Then the render() method is invoked. That method takes the HTTP request as the first argument, the template to be used as the second argument, and then a dictionary containing the data to be used in the template. (Nashville Software School, Ch 3 Documentation)
         return render(request, template, context)
