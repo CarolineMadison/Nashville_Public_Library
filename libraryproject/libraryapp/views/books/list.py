dfdc49aa58b2cdb271453164dcb4b93dab3421cb
Caroline Brownlee <brownleecaroline@gmail.com>
2020-02-06 19:08:59 -0600

    created view for books_list() and imported it into /views/ __init__.py

---
+++ libraryproject/libraryapp/views/books/list.py
@@ -0,0 +1,53 @@
+import sqlite3
+from django.shortcuts import render
+from libraryapp.models import Book
+from ..connection import Connection
+
+
+def book_list(request):
+    if request.method == 'GET':
+        with sqlite3.connect(Connection.db_path) as conn:
+            # sqlite3.Row allows developers to access columns in each row in the dataset by the column name instead of by index in the tuple. (Nashville Software School, Ch. Documentation)
+            conn.row_factory = sqlite3.Row
+            db_cursor = conn.cursor()
+
+            db_cursor.execute("""
+            select
+                b.id,
+                b.title,
+                b.isbn_number,
+                b.author,
+                b.year_published,
+                b.librarian_id,
+                b.location_id
+            from libraryapp_book b
+            """)
+
+            # When you instruct the sqlite3 package to fetchall(), it takes your SQL string and walks over to the database and executes it. It then takes all of the rows that the database generates, and creates a tuple out of each one. It then puts all of those tuples into a list. (Chapter Documentation, NSS)
+
+            # TUPLE is a collection which is ordered and unchangeable. Allows duplicate members. Parenthesis. (W3 Schools)
+
+            # LIST is a collection which is ordered and changeable. Allows duplicate members. Brackets. (W3 Schools)
+
+            all_books = []
+            dataset = db_cursor.fetchall()
+
+            for row in dataset:
+                # creating an instance of a book with Book()
+                book = Book()
+                book.id = row['id']
+                book.title = row['title']
+                book.isbn_number = row['isbn_number']
+                book.author = row['author']
+                book.year_published = row['year_published']
+                book.librarian_id = row['librarian_id']
+                book.location_id = row['location_id']
+
+                all_books.append(book)
+
+        template = 'books/list.html'
+        context = {
+            'all_books': all_books
+        }
+
+        return render(request, template, context)
